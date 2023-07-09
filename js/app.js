0136e5029afda60e0103ca26f247628f4626dbb9
Nathan Vasconcelos <nathanvasconcelos87@gmail.com>
2023-07-09 13:24:53 -0300

    criado a manipulacao de string para criar o link embed

--- js/app.js
+++ js/app.js
@@ -5,8 +5,11 @@ function carregarVideo(){
     const link = document.getElementById("barraDePesquisa").value;
     console.log("carregando vídeo");
     console.log(link);
+    console.log(link.substr(16, link.length));
+    const linkEmbed = 'https://www.youtube.com/embed' + link.substr(16, link.length);
+    console.log(linkEmbed);
     divPai.innerHTML =
-    `<iframe width="100%" height="72%" src="https://www.youtube.com/embed/${link}"
+    `<iframe width="100%" height="72%" src="${linkEmbed}"
     title="YouTube video player" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen class="video"></iframe>`
